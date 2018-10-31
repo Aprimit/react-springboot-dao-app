33bb248d3c9b13d8055b4e73b8e739ae5a7571c9
Aprimit Garg <aprimit.garg@aexp.com>
2018-10-31 20:16:42 +0530

    Use HttpStatus in Spring Controllers

--- userManagement/src/main/java/com/e2e/userManagement/controller/AddUserController.java
+++ userManagement/src/main/java/com/e2e/userManagement/controller/AddUserController.java
@@ -1,8 +1,8 @@
 package com.e2e.userManagement.controller;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -18,7 +18,7 @@ public class AddUserController {
 	AddUserService addUserSvc;
 
 	@RequestMapping(method = RequestMethod.POST, value = "userManagement/addUser")
-	public UserPojo addUser(@RequestBody UserPojo user) {
-		return addUserSvc.addUser(user);
+	public ResponseEntity<?> addUser(@RequestBody UserPojo user) {
+		return new ResponseEntity<>(addUserSvc.addUser(user), HttpStatus.CREATED);
 	}
 }
--- userManagement/src/main/java/com/e2e/userManagement/controller/DeleteUserController.java
+++ userManagement/src/main/java/com/e2e/userManagement/controller/DeleteUserController.java
@@ -1,6 +1,8 @@
 package com.e2e.userManagement.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -10,12 +12,13 @@ import com.e2e.userManagement.service.DeleteUserService;
 
 @RestController
 public class DeleteUserController {
-	
+
 	@Autowired
 	DeleteUserService deleteUserSvc;
 
 	@RequestMapping(value = "${spring.application.name}/deleteUser/{username}", method = RequestMethod.DELETE)
-	public void deleteUser(@PathVariable String username) {
+	public ResponseEntity<?> deleteUser(@PathVariable String username) {
 		deleteUserSvc.deleteUser(username);
+		return new ResponseEntity<>(HttpStatus.NO_CONTENT);
 	}
 }
