3299be242c34b93b7bf8ba7fc6b0a854b858a3a8
Aprimit Garg <aprimit.garg@aexp.com>
2018-12-29 17:56:43 +0530

    Add sql file for initial data setup in H2

---
+++ userManagement/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into users values('Jessica@pearson.com','Jessica', 'P', 'ABC','XYZ','MNO');
+insert into users values('mike.ross@pearson.com','Mike', 'Ross', 'Pearson Hardman','Law','Associate');
+insert into users values('harvey.v@spearson.com','Harvey', 'Ross', 'Pearson Hardman','Law','Senior Partner');
